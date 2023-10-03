d31f97ad45cf8281397c683e9c1033e4a3e1034f
Gashi87 <shayan_koyi@hotmail.com>
2023-10-03 13:26:15 +0200

    Class Added GitDemo2

---
+++ src/GitDemo2.java
@@ -0,0 +1,7 @@
+public class GitDemo2 {
+    public static void main(String[] args) {
+        String name = "Shayan";
+        String surename = "Ghefouri";
+        System.out.println(name + surename);
+    }
+}
